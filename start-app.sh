f078941f00b77b57845c3ff24cad29612ad2f0e2
Sinchan G <24f2008341@ds.study.iitm.ac.in>
2026-06-23 21:03:10 +0530

    opt: added project startup script

--- start-app.sh
+++ start-app.sh
@@ -1,3 +1,74 @@
 #!/usr/bin/bash 
 
+ESC=$(printf '\033')
 
+# Color Mapping ANSI
+NC="${ESC}[0m"
+WHITE="${ESC}[0;37m"
+RED="${ESC}[0;31m"
+GREEN="${ESC}[0;32m"
+YELLOW="${ESC}[0;33m"
+CYAN="${ESC}[0;36m"
+BOLD_CYAN="${ESC}[1;36m"
+BOLD_RED="${ESC}[1;31m"
+BOLD_GREEN="${ESC}[1;32m"
+DIM_WHITE="${ESC}[2;37m"
+
+cleanup() {
+    echo -e "\n Shutting down all services..."
+    kill $(jobs -p) 2>/dev/null
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+echo "${BOLD_CYAN}Starting Project..${NC}"
+sleep 2
+
+echo "${WHITE}---------------------------------------------------${NC}"
+echo "${DIM_WHITE}Firing up backend..${NC}"
+cd backend 
+uv run app.py &>/dev/null &
+BACKEND_PID=$!
+cd ..
+sleep 2
+
+echo "${WHITE}---------------------------------------------------${NC}"
+echo "${DIM_WHITE}Populating Database...${NC}"
+cd backend
+uv run utils/seed.py
+cd ..
+sleep 2
+
+echo "${WHITE}---------------------------------------------------${NC}"
+echo "${DIM_WHITE}Starting Frontend...${NC}"
+cd frontend 
+bun run dev &>/dev/null &
+FRONTEND_PID=$!
+cd ..
+
+sleep 5
+
+# Health Check
+BACKEND_RUNNING=$(ps -p $BACKEND_PID -o state= 2>/dev/null)
+FRONTEND_RUNNING=$(ps -p $FRONTEND_PID -o state= 2>/dev/null)
+
+if [ -z "$BACKEND_RUNNING" ]; then 
+    echo "${BOLD_RED}Backend failed to start!${NC}"
+    cleanup
+fi
+
+if [ -z "$FRONTEND_RUNNING" ]; then 
+    echo "${BOLD_RED}Frontend failed to start!${NC}"
+    cleanup
+fi
+
+clear
+echo "${BOLD_CYAN}---------------------------------------------------${NC}"
+echo "${BOLD_CYAN}Both services have been started succesfully!${NC}"
+echo "${BOLD_GREEN}Check http://localhost:5173 in your browser.${NC}"
+echo "${BOLD_CYAN}---------------------------------------------------${NC}"
+
+while true; do
+    sleep 1
+done
