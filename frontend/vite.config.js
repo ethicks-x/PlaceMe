b6b05ca7c24e739ec5201aa90464a83f50382ea6
Sinchan G <24f2008341@ds.study.iitm.ac.in>
2026-06-25 14:33:37 +0530

    feat: config bootstrap-vue & unplugin-vue-component

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { BootstrapVueNextResolver } from "bootstrap-vue-next";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dirs: ["src/components"],
+      extensions: ["vue"],
+      deep: true,
+      resolvers: [
+        BootstrapVueNextResolver(),
+        IconsResolver({
+          prefix: "i", // all icons Components will start with <i-...>
+          enabledCollections: ["bi"], // 'bi' stands for bootstrap-icons
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
 });
